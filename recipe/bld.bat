93916f1e5e63c64bcbb3c92b18783bdf5657a5cd
Tobias Diez <code@tobiasdiez.de>
2025-05-31 22:46:56 +0800

    clean up bld and metadata

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,21 +3,9 @@
 mkdir build
 cd build
 
-:: Configure
-cmake -G "NMake Makefiles" ^
-    %CMAKE_ARGS% ^
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    -DECL_WIN64=1 ^
-    %SRC_DIR%
-    ..
-if errorlevel 1 exit 1
-
 :: Build
 nmake
 if errorlevel 1 exit 1
-
 
 :: Install
 nmake install
