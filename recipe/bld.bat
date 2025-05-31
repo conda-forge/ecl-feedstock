29bf821dcecc2d57085255fa66b90b6b98610c6a
Tobias Diez <code@tobiasdiez.de>
2025-06-01 00:28:56 +0800

    Run tests directly from msvc/tests

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -12,5 +12,8 @@ if errorlevel 1 exit 1
 
 :: Test
 :: Needs to be run after installation, see https://gitlab.com/embeddable-common-lisp/ecl/-/issues/342
-:: Disabled as it fails due to missing ansi-tests
+:: The following fails due to missing ansi-tests, so run tests directly
 :: nmake /f Makefile test
+cd tests
+nmake /f Makefile check
+if errorlevel 1 exit 1
